7b5c0cc9cbbcc4675810b0b0cd2150431a74cec3
Arun Sandilya <50144683+arunsandy1309@users.noreply.github.com>
2024-12-17 21:29:55 -0500

    Updated the Layout.jsx
    Updated the Layout.jsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import { Stack } from "expo-router";
 import {useFonts} from "expo-font";
-
+import {CreateTripContext} from '../context/create_trip_context'
+import { useState } from "react";
 export default function RootLayout() {
 
   useFonts({
@@ -9,11 +10,16 @@ export default function RootLayout() {
     'outfit-bold': require('./../assets/fonts/Outfit-Bold.ttf')
   })
 
+const [tripData, setTripData] = useState([]);
+
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{
-        headerShown: false
-      }}/>
+    <CreateTripContext.Provider value={{tripData, setTripData}}>
+    <Stack screenOptions={{headerShown: false,}}>
+      {/* <Stack.Screen name="index" options={{
+        headerShown: false,
+      }}/> */}
+      <Stack.Screen name="(tabs)"/>
     </Stack>
+    </CreateTripContext.Provider>
   );
 }
